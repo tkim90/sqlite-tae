365722dc96ac4d1ce5549e3e058ab6f9d7625ca9
Tae Kim <taehnkim@gmail.com>
2024-12-30 22:39:11 -0500

    rename

--- main.c
+++ main.c
@@ -150,17 +150,17 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
   return META_COMMAND_UNRECOGNIZED_COMMAND;
 }
 
-PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement_ptr) {
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
   if (strcmp(input_buffer->buffer, "select") == 0) {
-    statement_ptr->type = STATEMENT_SELECT;
+    statement->type = STATEMENT_SELECT;
     return PREPARE_SUCCESS;
   }
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-    statement_ptr->type = STATEMENT_INSERT;
+    statement->type = STATEMENT_INSERT;
     int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s",
-                               &(statement_ptr->row_to_insert.id),
-                               &(statement_ptr->row_to_insert.username),
-                               &(statement_ptr->row_to_insert.email));
+                               &(statement->row_to_insert.id),
+                               &(statement->row_to_insert.username),
+                               &(statement->row_to_insert.email));
     if (args_assigned < 3) {
       return PREPARE_SYNTAX_ERROR;
     }
