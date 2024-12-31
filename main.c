a8aee69af104901d37538a50daa682693b48ae08
Tae Kim <taehnkim@gmail.com>
2024-12-31 00:27:06 -0500

    lots of comments

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,9 +21,10 @@ typedef enum {
 } PrepareResult;
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
 
-// Called "preprocessor directives": performs text replacement before code compiles.
-// aka every instance of COLUMN_USERNAME_SIZE is replaced with 32
-// this way, it doesn't take up memory in your program; the final code is just the values
+// Called "preprocessor directives": performs text replacement before code
+// compiles. aka every instance of COLUMN_USERNAME_SIZE is replaced with 32 this
+// way, it doesn't take up memory in your program; the final code is just the
+// values
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
 
@@ -62,33 +64,46 @@ const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
-// SERIALIZE & DESERIALIZE
-// void* (void pointer) is a pointer to a generic type (any)
-// generally unsafe but typically used in serialization
-// I should still carefully type cast this at runtime
-// If `destination` were to accept 1 of 2 types, we could use:
-//   1. - (Struct*)0    creates a NULL pointer. By casting a 0 t Function
-//   overloading ->
-//    serialize_row_int(Row* source, int* destination) or
-//    serialize_row_double(Row* source, double* destination)
-//   2. Tagged Union (Discriminated Union)
-//    typedef enum {
-//     DESTINATION_INT,
-//     DESTINATION_DOUBLE,
-//   } DestinationType;
-//
-//   typedef struct {
-//     DestinationType type;
-//     union {
-//       int* destination_int;
-//       double* destination_double;
-//     } destination
-//   } Destination;
+/*
+ * SERIALIZE & DESERIALIZE
+ *
+ * void* (void pointer) is a pointer to a generic type (any)
+ * generally unsafe but typically used in serialization
+ *
+ * I should still carefully type cast this at runtime
+ *
+ * If `destination` were to accept 1 of 2 types, we could use:
+ *   1. (Struct*)0    creates a NULL pointer. By casting a 0 to a function
+ *     overloading ->
+ *     serialize_row_int(Row* source, int* destination) or
+ *     serialize_row_double(Row* source, double* destination)
+ *
+ *   2. Tagged Union (Discriminated Union)
+ *     typedef enum {
+ *       DESTINATION_INT,
+ *       DESTINATION_DOUBLE,
+ *     } DestinationType;
+ *
+ *     typedef struct {
+ *       DestinationType type;
+ *       union {
+ *         int* destination_int;
+ *         double* destination_double;
+ *       } destination
+ *     } Destination;
+ */
 void serialize_row(Row *source, void *destination) {
-  // memcp copies n chars from src to dest.
-  /* void *memcpy(void *dst, const void *src, size_t n) */
-  // ^^ notice the `const void *src` syntax
-  // `const` is a hint that *src shouldn't be modified.
+  /*
+   * memcp copies n chars from src to dest.
+   *
+   * void *memcpy(void *dst, const void *src, size_t n)
+   * ^^ notice the `const void *src` syntax
+   * `const` is a hint that *src shouldn't be modified.
+   *
+   *
+   * memcpy(destination address, source address to copy bytes from, number of
+   * bytes to copy)
+   */
   memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
   memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
   memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
@@ -102,6 +117,10 @@ void deserialize_row(void *source, Row *destination) {
 
 // Table structure that points to pages of rows and keeps tracks of how many
 // rows there are in prepare_statement
+
+// 4KB; equivalent to a page in most VMs.
+// This way 1 page in our DB is 1 page in the OS, so it's treated as a single
+// unit
 const uint32_t PAGE_SIZE = 4096;
 #define TABLE_MAX_PAGES 100
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
@@ -109,6 +128,7 @@ const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 typedef struct {
   uint32_t num_rows;
+  void *pages[TABLE_MAX_PAGES]; // array of pointers
 } Table;
 
 void print_prompt() { printf("db > "); }
@@ -180,6 +200,41 @@ PrepareResult prepare_statement(InputBuffer *input_buffer,
   }
 
   return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+// Figures out where to read/write a particular row in memory
+void *read_slot(Table *table, uint32_t row_num) {
+  // Calculate which page contains this row
+  uint32_t page_num = row_num / ROWS_PER_PAGE;
+
+  // If the page doesn't exist yet, create it
+  if (table->pages[page_num] == NULL) {
+    table->pages[page_num] = malloc(PAGE_SIZE);
+  }
+
+  // Get a pointer to the start of the page
+  void *page_start = table->pages[page_num];
+
+  /*
+   * get row position by doing modulo %
+   * it's perfect for getting a "position"
+   *
+   * say ROWS_PER_PAGE = 10.
+   * if row_num is 0-9, % 10 will be those numbers (0-9)
+   * if row_num is 11, % 10 = 1 (first position in next page)
+   * if row_num is 15, % 10 = 5 (fifth position in next page)
+   * if row_num is 25, % 10 = 5 (fifth position in third page)
+   *
+   * So row_position_in_page always gives you the row's position within its specific page,
+   * regardless of which page it's on.
+   */
+  uint32_t row_position_in_page = row_num % ROWS_PER_PAGE;
+
+  // `row_position_in_page * ROW_SIZE` calculates how many bytes you need to
+  // move from the start of the page to its specific row
+  void *row_location = page_start + (row_position_in_page * ROW_SIZE);
+
+  return row_location;
 }
 
 void execute_statement(Statement *statement) {
